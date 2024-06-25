50b71164f71556a440418a745efff2dd8c5136df
ASZhidenko <aszhidenko@mai.education>
2024-06-25 20:03:48 +0300

    1

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ async def plot_regression():
             ]
         )
 
-    fig = make_subplots(rows=2, cols=2, subplot_titles=[i[0] for i in graphs])
+    fig = make_subplots(rows=4, cols=1, subplot_titles=[i[0] for i in graphs])
 
     for i in range(2):
         fig.add_trace(
@@ -100,18 +100,18 @@ async def plot_regression():
         )
         fig.add_trace(
             graphs[1][i + 1],
-            row=1, 
-            col=2
+            row=2,
+            col=1
         )
         fig.add_trace(
             graphs[2][i + 1],
-            row=2, 
+            row=3,
             col=1
         )
         fig.add_trace(
             graphs[3][i + 1],
-            row=2, 
-            col=2
+            row=4,
+            col=1
         )
 
     fig.update_layout(
@@ -119,77 +119,8 @@ async def plot_regression():
         margin=dict(l=0, r=0, t=40, b=0)
     )
 
-
     # Convert the plot to HTML
-    plot_div = to_html(fig, full_html=False)
+    plot_div = to_html(fig, full_html=True)
 
-    return HTMLResponse(content=generate_page(plot_div))
+    return HTMLResponse(content=plot_div)
 
-
-@app.get("/regression", response_class=HTMLResponse)
-async def plot_regression(engine_type: str, side: Literal["left", "right"]):
-        
-    side = 1 if side == "left" else 2
-    # Чтение данных
-    X_train = pd.read_csv('./data/X_train.csv', parse_dates=['reportts'])
-    y_train = pd.read_csv('./data/y_train.csv', parse_dates=['reportts'])
-    X_test = pd.read_csv('./data/X_test.csv', parse_dates=['reportts'])
-
-    dataset = X_train.merge(y_train, on=['acnum', 'pos', 'reportts']).dropna(subset=['egtm'])
-
-    # Обучение моделей для каждого из четырех графиков
-    fleet = ['VQ-BGU', 'VQ-BDU']
-    positions = [1, 2]
-    results = {}
-
-    for acnum, pos in itertools.product(fleet, positions):
-        key = f'{acnum}_pos_{pos}'
-        X = dataset[(dataset['acnum'] == acnum) & (dataset['pos'] == pos)].drop(columns=['egtm'])
-        y = dataset[(dataset['acnum'] == acnum) & (dataset['pos'] == pos)]['egtm']
-        rmse, mae, model, indices, predictions = train_model(X, y)
-        print(predictions)
-        results[key] = (rmse, mae, indices, predictions)
-        print(f'{key} RMSE={rmse:.3f} MAE={mae:.3f}')
-
-    # Чтение данных для графиков
-    df = pd.read_csv('data/y_train.csv')
-    egtm_column_name = df.columns[3]
-    acnum_column_name = 'acnum'
-    pos_column_name = df.columns[2]
-    graphs = []
-    for i, (label, group) in enumerate(results.items(), 1):
-        if label != f"{engine_type}_pos_{side}":
-            continue
-        rmse, mae, indices, predictions = group
-        data_group = df[(df[acnum_column_name] == label.split('_')[0]) & (df[pos_column_name] == int(label.split('_')[2]))]
-
-        # Убедиться, что размеры совпадают
-        val_start_idx = len(data_group) - len(predictions)
-        graphs.append(
-            [
-                f'{label} RMSE={rmse:.3f} MAE={mae:.3f}',
-                go.Scatter(
-                    y=data_group[egtm_column_name].values, 
-                    name=f'{label}'
-                ),
-                go.Scatter(
-                    x=list(range(val_start_idx, len(data_group))),
-                    y=predictions,
-                    name=f'{label} Predicted',
-                ),
-            ]
-        )
-
-    if not graphs:
-        raise HTTPException(404, "Not found this engine")
-
-    fig = make_subplots(rows=2, cols=2, subplot_titles=[graphs[0][0]])
-    fig.add_trace(
-        graphs[0][1],
-        row=1, 
-        col=1
-        )
-
-    plot_div = to_html(fig, full_html=False)
-
-    return HTMLResponse(content=generate_page(plot_div))
