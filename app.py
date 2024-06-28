fd612abee9db4eb03528fcba4682e0aeaf0e5c70
Mikita-Arsi <ardonnik@gmail.com>
2024-06-28 21:41:55 +0300

    prod with html

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-
+from fastapi.responses import HTMLResponse, FileResponse
 from graphs import graphs_router
 
 
 app = FastAPI()
 
-
 @app.get("/", response_class=HTMLResponse)
 async def main_page():
-    return ...
+    return FileResponse('static/index.html')
 
 app.include_router(graphs_router)
-
